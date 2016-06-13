e1104ecedc78bd1d72fc66c3e17252cb1fc8dc0c
mars <mars@huoban.com>
2016-06-13 15:43:55 +0800

    no message

---
+++ src/components/selector.vue
@@ -0,0 +1,32 @@
+<template>
+  <select v-model='value' class="form-control">
+    <option v-for='option of options' :value='option.value'>{{option.label}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+
+  name: 'selector',
+
+  props: {
+    options: {
+      type: Array,
+      default: []
+    }
+  },
+
+  data() {
+    return {
+      value: 1
+    };
+  },
+
+  created() {
+    console.log(this.options)
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
