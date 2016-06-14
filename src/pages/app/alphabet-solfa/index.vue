b5587e64d3516b87e0adf391837a594deede2f8d
mars <mars@huoban.com>
2016-06-14 18:07:50 +0800

    no message

--- src/pages/app/alphabet-solfa/index.vue
+++ src/pages/app/alphabet-solfa/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    alphabet-solfa
     <router-view></router-view>
   </div>
 </template>
--- src/pages/app/alphabet-solfa/tests/index.vue
+++ src/pages/app/alphabet-solfa/tests/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    test
     <router-view></router-view>
   </div>
 </template>
