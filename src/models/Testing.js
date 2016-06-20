712a661d856b71e57ea69b22dcd6f879961f4fba
mars <mars@huoban.com>
2016-06-20 14:34:35 +0800

    update constructor

--- src/models/Testing.js
+++ src/models/Testing.js
@@ -1,7 +1,14 @@
+import * as utils from 'utils'
+
+export const TestingTypes = {
+  'alphabetSolfa': 'alphabet-solfa'
+}
+
 export default class Testing {
-  constructor(config) {
+  constructor(type, config) {
+    this.id = 1
+    this.type = type
     this.config = config
     this.items = []
-
   }
 }
