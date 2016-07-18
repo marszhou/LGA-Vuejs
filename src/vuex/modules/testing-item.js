19855aebefcc846759a0112e9b22e17966d8234b
mars <mars@huoban.com>
2016-07-18 16:08:01 +0800

    选题后保存testing

--- src/vuex/modules/testing-item.js
+++ src/vuex/modules/testing-item.js
@@ -1,6 +1,8 @@
 import * as types from '../mutation-types'
 import _ from 'lodash'
 
+import Testing from 'models/testing'
+
 const {
   TESTING_ITEM_GET
 } = types.testingItem
@@ -12,6 +14,7 @@ const state = {
 const mutations = {
   [TESTING_ITEM_GET](state, testing, index) {
     state.item = testing.getItemAt(index)
+    Testing.save(testing)
   }
 }
 
