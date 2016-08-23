d4cdd439a29e1cb9396ed73fe3580b7d8ef2639f
mars <mars@huoban.com>
2016-08-23 12:56:43 +0800

    progress bar

---
+++ src/components/progress-bar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="progress">
+    <div class="progress-bar progress-bar-success" role="progressbar" :aria-valuenow="current" :aria-valuemin="min" :aria-valuemax="max" :style="{width: percent + '%'}">
+      <span class="sr-only">{{percent}}%</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'component_name',
+
+  components: {
+
+  },
+
+  props: {
+    min: {
+      type: Number,
+      default: 0
+    },
+    max: {
+      type: Number,
+      default: 100
+    },
+    current: {
+      type: Number,
+      default: 10
+    }
+  },
+
+  vuex: {
+    actions: {
+
+    },
+    getters: {
+
+    }
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  computed: {
+    percent() {
+      return Math.round((this.current-this.min) / (this.max-this.min) * 10000) / 100
+    }
+  },
+
+  methods: {
+
+  },
+
+  route: {
+
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
