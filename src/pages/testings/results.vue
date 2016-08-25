5a086dfac52e6ba73536a43a2538283f6f1e3434
mars <mars@huoban.com>
2016-08-25 14:26:24 +0800

    testing result

--- src/pages/testings/results.vue
+++ src/pages/testings/results.vue
@@ -1,22 +1,49 @@
 <template>
-  <div></div>
+
+  <div v-if='testing'>
+    <h2>测试 - {{testing.type.display}} - 查看结果</h2>
+    <testing-info :testing='testing' ></testing-info>
+    <table class="table table-striped table-bordered">
+      <tr>
+        <th><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></th>
+        <th><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span></th>
+        <th><span class="glyphicon glyphicon-hand-down" aria-hidden="true"></span></th>
+        <th></th>
+      </tr>
+      <tr v-for='item of testing.items'>
+        <td>
+          <strong>({{item.question.name}})</strong> {{item.question.title}}
+        </td>
+        <td>
+          <span style='color: darkgreen; font-weight: bold'>{{item.question.corrects}}</span>
+        </td>
+        <td>
+          {{item.answer.selection}}
+        </td>
+        <td>
+          <span class="glyphicon glyphicon-ok" aria-hidden="true" v-if='item.answer.result' style='color: darkgreen'></span>
+          <span class="glyphicon glyphicon-remove" aria-hidden="true" v-if='!item.answer.result' style='color:red'></span>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
+import TestingActions from 'actions/testing'
+import TestingInfo from 'components/testing-info'
 export default {
 
   name: 'testing-results',
 
   components: {
-
+    TestingInfo
   },
 
   vuex: {
-    actions: {
-
-    },
+    actions: TestingActions,
     getters: {
-
+      testing: state => state.testing.current
     }
   },
 
@@ -36,6 +63,9 @@ export default {
 
   route: {
 
+  },
+  attached() {
+    this.setCurrent(this.$route.params.testing_id)
   }
 };
 </script>
