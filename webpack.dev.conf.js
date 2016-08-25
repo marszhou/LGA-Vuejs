3b065761ea7281d5f1c0e045bd346f46548e927f
mars <mars@huoban.com>
2016-08-25 14:49:22 +0800

    Updates

---
+++ webpack.dev.conf.js
@@ -0,0 +1,62 @@
+var config = require('../config')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var utils = require('./utils')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var path = require('path')
+
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+console.log(__dirname)
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    loaders: utils.styleLoaders()
+  },
+  // eval-source-map is faster for development
+  devtool: '#inline-source-map',
+  output: {
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]',
+    devtoolModuleFilenameTemplate: info => {
+      if (info.resource.match(/\.vue$/)) {
+        $filename = info.allLoaders.match(/type=script/)
+                  ? info.resourcePath : 'generated';
+      } else {
+        $filename = info.resourcePath;
+      }
+      return $filename;
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin([
+      {
+        context: path.resolve(__dirname, '../src'),
+        from: {
+          glob: 'assets/**/*',
+          dot: true
+        },
+        to: 'static'
+      }
+    ])
+  ]
+})
