60941a1e9cdfaa9a3a8eae1507dc11e40460df6e
mars <mars@huoban.com>
2016-11-23 18:06:05 +0800

    no message

---
+++ src/pages/musical-theory/chords.vue
@@ -0,0 +1,44 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+  name: 'component_name',
+
+  components: {
+
+  },
+
+  vuex: {
+    actions: {
+
+    },
+    getters: {
+
+    }
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+
+  route: {
+
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
